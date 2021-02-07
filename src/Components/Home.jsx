4f92bd5727d6fe30ccd2701edd6e9d4b5048add0
Ratul <azammmgol@gmail.com>
2021-02-07 16:17:33 +0600

    new update done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,13 +8,11 @@ import Footer from './Footer'
 import Subscribe from './Subscribe'
 import Review from './Review'
 import Blog from './Blog'
-// import Loader from './Loader'
 
 const Home = () => {
   document.title = ' DevR - Home '
   return (
     <>
-      {/* <Loader /> */}
       <Header />
       <ShortAbout />
       <ShortProject />
